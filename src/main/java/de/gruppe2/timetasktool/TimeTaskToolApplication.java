a2d6d3936f11af9aae0edac3759d2e499b26a121
Sebastian <Seb_Schweikl@web.de>
2018-11-05 20:03:27 +0100

    Directories and Spring Boot Application Setup Files

---
+++ src/main/java/de/gruppe2/timetasktool/TimeTaskToolApplication.java
@@ -0,0 +1,12 @@
+package de.gruppe2.timetasktool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTaskToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTaskToolApplication.class, args);
+	}
+}
