2664b116b6ad695565f3d8ed247655d64ab15ee8
Sebastian <Seb_Schweikl@web.de>
2018-11-05 20:34:47 +0100

    Changed various files so that the project is not standalone anymore and needs to be deployed on a server.

--- src/main/java/de/gruppe2/timetasktool/TimeTaskToolApplication.java
+++ src/main/java/de/gruppe2/timetasktool/TimeTaskToolApplication.java
@@ -2,9 +2,10 @@ package de.gruppe2.timetasktool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TimeTaskToolApplication {
+public class TimeTaskToolApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimeTaskToolApplication.class, args);
---
+++ src/main/java/de/gruppe2/timetasktool/controller/IndexController.java
@@ -0,0 +1,12 @@
+package de.gruppe2.timetasktool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
