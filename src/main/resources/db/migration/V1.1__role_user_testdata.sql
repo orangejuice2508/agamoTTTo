fa45950f096513bc384e54a7d6a6f11c12e7f521
Sebastian <Seb_Schweikl@web.de>
2018-12-01 12:49:26 +0100

    Added Flyway for DB migration

---
+++ src/main/resources/db/migration/V1.1__role_user_testdata.sql
@@ -0,0 +1,11 @@
+INSERT INTO `role` VALUES 
+(1,'ROLE_ADMINISTRATOR'),
+(3,'ROLE_MITARBEITER'),
+(2,'ROLE_VORGESETZTER');
+
+INSERT INTO `agamottto_user` VALUES 
+(1,'Adm','Inistrator','adm@inistrator.de','$2a$10$JNX2Z4h4gC6m/w/VEZOt8eODBCMHDa79fxOtffnxisRXM.6KGLd3y',1,1),
+(2,'Vor','Gesetzter','vor@gesetzter.de','$2a$10$NDw.yKoCOpTPleZ.dgzOPubMRWWOWrj.xHKe1D408D3J1ierF1B9S',1,2),
+(3,'Mit','Arbeiter','mit@arbeiter.de','$2a$10$xqDqHyxUdhcyCmFjy3usbuAB7C3s207emtCC1su1yoBHn6kUX5lCG',1,3);
+
+
