41be5a9c02137d4ac65213fe202ebbaa87dd88b8
Sebastian <Seb_Schweikl@web.de>
2018-12-03 18:30:27 +0100

    Adding pools is now possible

---
+++ src/main/java/de/gruppe2/agamoTTTo/entity/Pool.java
@@ -0,0 +1,31 @@
+package de.gruppe2.agamoTTTo.entity;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@Table(name = "pool")
+@EntityListeners(AuditingEntityListener.class)
+public class Pool {
+    @Id
+    @GeneratedValue
+    @Column(name = "pool_id")
+    private Long id;
+
+    @Size(max = 100)
+    @NotEmpty
+    @Column(name = "pool_name", unique = true)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "owner", nullable = false)
+    @CreatedBy
+    private User owner;
+}
