722c76bd9c9ae4a7b19ff04b93dc5efebda8556b
Hifl-coding <nowakjohannes1995@gmail.com>
2018-12-11 13:58:25 +0100

    hifl commit

--- src/main/java/de/gruppe2/agamoTTTo/controller/RecordController.java
+++ src/main/java/de/gruppe2/agamoTTTo/controller/RecordController.java
@@ -1,11 +1,9 @@
 package de.gruppe2.agamoTTTo.controller;
 
 import de.gruppe2.agamoTTTo.domain.entity.Pool;
-import de.gruppe2.agamoTTTo.domain.entity.Record;
-import de.gruppe2.agamoTTTo.domain.entity.User;
 import de.gruppe2.agamoTTTo.security.Permission;
+import de.gruppe2.agamoTTTo.service.PoolService;
 import de.gruppe2.agamoTTTo.service.RecordService;
-import de.gruppe2.agamoTTTo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -19,8 +17,10 @@ import javax.validation.Valid;
 import java.util.Locale;
 
 @Controller
-@RequestMapping("records")
+@RequestMapping("pools")
 public class RecordController extends de.gruppe2.agamoTTTo.controller.Controller{
+
+    private PoolService poolService;
 
     private RecordService recordService;
 
@@ -28,7 +28,7 @@ public class RecordController extends de.gruppe2.agamoTTTo.controller.Controller
 
     @Autowired
     public RecordController(RecordService recordService, MessageSource messageSource) {
-        this.recordService = recordService;
+        this.poolService = poolService;
         this.messageSource = messageSource;
 
     }
@@ -39,10 +39,33 @@ public class RecordController extends de.gruppe2.agamoTTTo.controller.Controller
      * @param model the Spring Model
      * @return path to template
      */
-     @PreAuthorize(Permission.MITARBEITER)
-     @GetMapping("/add")
-     public String getAddRecordsPage(Model model) {
-         model.addAttribute("record", new Record());
-         return "record/add";
-     }
+    public String getAddPoolsPage(Model model) {
+        model.addAttribute("pool", new Pool());
+        return "pools/add";
+    }
+
+    @PreAuthorize(Permission.VORGESETZTER)
+    @PostMapping("/add")
+    public String postAddPoolsPage(@ModelAttribute @Valid Pool pool, BindingResult bindingResult) {
+        /* If the form contains errors, the new pool won't be added and the form is displayed again with
+           corresponding error messages. */
+        if(bindingResult.hasErrors()) {
+            return "pools/add";
+        }
+
+        /* Try to add the pool to the database. If the pool name exists already, a DataIntegrityViolation
+        will be thrown by the PoolService/PoolRepository. Then the form is shown again with a corresponding
+        error message.
+        */
+        try {
+            poolService.addPool(pool);
+        }
+        catch(DataIntegrityViolationException e) {
+            bindingResult.rejectValue("name", "error.pool", messageSource.getMessage("pools.error.name_not_unique", null, Locale.getDefault()));
+            return "pools/edit";
+        }
+
+        // If the pool was added successfully, reload the page with an empty form.
+        return "redirect:/pools/add/?successful=true";
+    }
 }
