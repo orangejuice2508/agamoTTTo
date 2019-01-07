7b78c668764300235b34fdebf20c368318d13f80
Sebastian <Seb_Schweikl@web.de>
2019-01-07 18:53:29 +0100

    Improved various things

--- src/main/java/de/gruppe2/agamoTTTo/controller/RecordController.java
+++ src/main/java/de/gruppe2/agamoTTTo/controller/RecordController.java
@@ -11,7 +11,6 @@ import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
