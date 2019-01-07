e5fc2190d02bd067696cba41d59dec643626035c
Sebastian <Seb_Schweikl@web.de>
2019-01-07 16:44:16 +0100

    Changed sql file due to error when building it on the server

--- src/main/resources/db/migration/V1.8__alter_table_record_and_record_log.sql
+++ src/main/resources/db/migration/V1.8__alter_table_record_and_record_log.sql
@@ -2,5 +2,5 @@ ALTER TABLE agamoTTTo_db.record
   ADD COLUMN is_deleted BOOLEAN NOT NULL DEFAULT 0
   AFTER pool_id;
 
-ALTER TABLE agamottto_db.record_log
+ALTER TABLE agamoTTTo_db.record_log
   ADD CONSTRAINT `record_log_fk4` FOREIGN KEY (record_id) REFERENCES record (record_id);
