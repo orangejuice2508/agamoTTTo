de049af24b71d7d298e212bb0e709d9990883b4d
Sebastian <Seb_Schweikl@web.de>
2019-01-16 12:59:13 +0100

    Just some minor changes

--- src/main/resources/db/migration/V1.9__add_table_password_reset_roken.sql
+++ src/main/resources/db/migration/V1.9__add_table_password_reset_roken.sql
@@ -4,6 +4,6 @@ CREATE TABLE agamoTTTo_db.password_reset_token (
   user_id                 BIGINT      NOT NULL,
   expiry_date             TIMESTAMP   NOT NULL,
   PRIMARY KEY (password_reset_token_id),
-  KEY password_reset_token_fk_idx (user_id),
-  CONSTRAINT password_reset_token_fk FOREIGN KEY (user_id) REFERENCES agamoTTTo_user (user_id)
+  KEY `password_reset_token_fk_idx` (user_id),
+  CONSTRAINT `password_reset_token_fk` FOREIGN KEY (user_id) REFERENCES agamoTTTo_user (user_id)
 );
